cebc6d8444d1359113151474d308e893e108312c
PabloMD <chmielecki7@gmail.com>
2021-03-14 19:52:13 +0100

    nicer way to pass parameters to inner component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,12 +4,12 @@ import './Card.css';
 
 
 function Card(props){
-    const { title, imageSrc } = props;
+    const { title, imageSrc, intro, content, showMore } = props;
     return(
         <div className="card">
          <CardTitle title={title} />
          <CardImage src={imageSrc} alt={title} />
-         <CardContent { ...props } />
+         <CardContent { ...{ intro, content, showMore } } />
         </div>
     );
 }
