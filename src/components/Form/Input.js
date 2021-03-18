57834ea1b94781a478649fd4b7564dbabb6a64ba
PabloMD <chmielecki7@gmail.com>
2021-03-18 18:11:10 +0100

    Missing Input.js update ?! + prettier config

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,18 +1,21 @@
-import { ColorPaletteMap } from "../Layout";
+import { ColorPaletteMap } from '../Layout';
 
-
-function Input({ label, bgColor = "nephritis", color = "wisteria", borderSize, borderRadius, borderColor}){
+function Input({
+    bgColor = 'nephritis',
+    color = 'wisteria',
+    borderSize,
+    borderRadius,
+    borderColor,
+}) {
     const styles = {
         backgroundColor: ColorPaletteMap[bgColor],
         color: ColorPaletteMap[color],
         borderWidth: borderSize,
         borderRadius: parseInt(borderRadius),
-        borderColor: ColorPaletteMap[borderColor]
+        borderColor: ColorPaletteMap[borderColor],
     };
 
-    return(
-        <input style={styles} />
-    );
+    return <input style={styles} />;
 }
 
 export default Input;
